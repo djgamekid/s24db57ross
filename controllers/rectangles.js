ea4cc6e370c83c99c64d92aa38618c42c6cc5359
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-04-05 20:41:03 -0500

    Part 1

--- controllers/rectangles.js
+++ controllers/rectangles.js
@@ -26,9 +26,17 @@ exports.rectangle_view_all_Page = async function(req, res) {
         
     
 // for a specific Rectangle.
-exports.rectangle_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Rectangle detail: ' + req.params.id);
+exports.rectangle_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Rectangle.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
+
 // Handle Costume create on POST.
 exports.rectangle_create_post = async function(req, res) {
 console.log(req.body)
