256b773aafd45c2b85277337f70d57e2371b21d5
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-04-05 20:32:15 -0500

    Part 7

--- controllers/rectangles.js
+++ controllers/rectangles.js
@@ -29,9 +29,24 @@ exports.rectangle_view_all_Page = async function(req, res) {
 exports.rectangle_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Rectangle detail: ' + req.params.id);
 };
-// Handle Rectangle create on POST.
-exports.rectangle_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Rectangle create POST');
+// Handle Costume create on POST.
+exports.rectangle_create_post = async function(req, res) {
+console.log(req.body)
+let document = new Rectangle();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"width":15.0, "height":8.0};
+document.width = req.body.width;
+document.height = req.body.height;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
 };
 // Handle Rectangle delete from on DELETE.
 exports.rectangle_delete = function(req, res) {
