b836b317919b01cbfb3bda3d3fb16da8f6632c56
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-04-17 15:40:14 -0500

    Assignment 13 part 6&7

--- models/rectangle.js
+++ models/rectangle.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
 const rectangleSchema = mongoose.Schema({
-    width: Number,
-    height: Number
-    
+    width: {
+        type: Number,
+        min: [1, 'width must at least be 1'],
+        max: 100000000
+    },
+    height: {
+        type: Number,
+        min: [1, 'width must at least be 1'],
+        max: 100000000
+    }
 })
 module.exports = mongoose.model("rectangle",
     rectangleSchema)
